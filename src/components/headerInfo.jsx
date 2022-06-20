debf5f4f1bf8e4e67f989dab7c35ce7191f3a99b
mark savka <mark.savka.it@gmail.com>
2022-06-20 15:31:43 +0300

    refactor time

--- src/components/headerInfo.jsx
+++ src/components/headerInfo.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 const HeaderInfo = ({ label }) => {
-	function getLocalDate() {
-		let date = new Date(Date());
+  function getLocalDate() {
+    let date = new Date(Date());
 
-		let options = {
-			year: "numeric",
-			day: "numeric",
-			month: "long",
-			weekday: "long",
-		};
-		return date.toLocaleString("en-US", options);
-	}
+    let options = {
+      year: "numeric",
+      day: "numeric",
+      month: "long",
+      weekday: "long",
+    };
+    return date.toLocaleString("uk-US", options);
+  }
 
-	return (
-		<div className="header-data-info">
-			<h1>{label}</h1>
-			<p>{getLocalDate()}</p>
-		</div>
-	);
+  return (
+    <div className='header-data-info'>
+      <h1>{label}</h1>
+      <p>{getLocalDate()}</p>
+    </div>
+  );
 };
 
 export default HeaderInfo;
